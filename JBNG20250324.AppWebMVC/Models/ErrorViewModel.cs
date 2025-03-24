b34044e14d8e0df0345ec98f5ea660b02ebd0bcd
Jasso <Jasso@DESKTOP-97AP7FC>
2025-03-24 07:47:31 -0600

    Agregar archivos de proyecto.

---
+++ JBNG20250324.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JBNG20250324.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
